15cba7f66c912fa48c94c758cfc31e13fce82bf9
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-30 21:25:17 +0530

    Added switch case statements for calulate wages for employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,7 +7,8 @@ public class EmployeeWage {
 	private static final int present=1;
 	private static final int absent=0;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		final int wagePerHour=20;
 		int fullTimePresent=2;
 		int partTimePresent=1;
@@ -18,23 +19,19 @@ public class EmployeeWage {
 		System.out.println("Welcome to employee wage computation program");
 		Random random=new Random();
 		int attendance=random.nextInt(3);
-		if(attendance == fullTimePresent)
+		switch(attendance)
 		{
-			System.out.println("Employee is present full time:"+attendance);
-	     	dailyEmpWage=fullTimeHour*wagePerHour;
-     		System.out.println("Daily Emp wage:" +dailyEmpWage);
-		}
-		else if(attendance == partTimePresent)
-		{
-			System.out.println("Employee is present half time:"+attendance);
-    		dailyEmpWage=partTimeHour*wagePerHour;
-    		System.out.println("Daily Emp Wage:"+dailyEmpWage);
-		}
-		else
-		{
-			System.out.println("Employee is absent:"+attendance);
-    		dailyEmpWage=absent*wagePerHour;
-      		System.out.println("Employee is absent:" +dailyEmpWage);
+	                case 1:
+               			int partTimeWage=(partTimeHour*wagePerHour);
+	                	System.out.println("Part Time Employee "+partTimeWage);
+	                	break;
+	                case 2:
+
+                		int fullTimeWage=(fullTimeHour*wagePerHour);
+                		System.out.println("Full Time Employee "+fullTimeWage);
+                		break;
+	                default:
+                		System.out.println("Employee Absent "+absent);
 		}
 	}
 
