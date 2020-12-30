9933e2520974cda81184484b3f2bd92d40186d43
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-30 21:00:29 +0530

    Added a check for employee attendace

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,26 @@
 package com.javaprogram.employeewage;
 
-public class EmployeeWage
-{
+
+import java.util.*;
+
+public class EmployeeWage {
+
+	private static final int present=1;
+	private static final int absent=0;
 
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage program");
-
+		System.out.println("Welcome to employee wage computation program");
+		Random random=new Random();
+		int attendance=random.nextInt(2);
+		if(attendance == present)
+		{
+			System.out.println("Present "+attendance);
+		}
+		else
+		{
+			System.out.println("absent "+attendance);
+		}
 	}
 
 }
